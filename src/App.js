f49df98eb175cc81d013d6e4a30bfa0bbf16e2ea
Turgay Levent <turgayleventt@gmail.com>
2022-11-28 14:53:18 +0300

    starter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <h1>Language Cards</h1>
+      <Header />
+      <Card />
     </div>
   );
 }
