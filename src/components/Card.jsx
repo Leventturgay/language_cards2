d66a57b79d9e4ae97f0881b91ee46f51c7c1ebe7
Turgay Levent <turgayleventt@gmail.com>
2022-11-30 11:53:56 +0300

    cardsComponent

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,8 +11,8 @@ const Card = () => {
       <Row>
         {data.map((lang, index) => {
           return (
-            <Col>
-              <Language {...lang} key={index} />;
+            <Col sm={6} md={4} lg={3} key={index}>
+              <Language {...lang} />;
             </Col>
           );
         })}
