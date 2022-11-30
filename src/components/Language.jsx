01ff08afe6a104f45d3875aee8a407b0ef43be7e
Turgay Levent <turgayleventt@gmail.com>
2022-11-30 14:45:25 +0300

    language2

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -3,22 +3,26 @@ import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-  const [showDesc, setShowDesc] = useState(false);
+  const [showImage, setShowImage] = useState(true);
   return (
     <Container
-      onClick={() => setShowDesc(!showDesc)}
+      onClick={() => setShowImage(!showImage)}
       style={{ background: "#ffdab9" }}
       className="p-4 rounded-4"
     >
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
-      <ul>
-        {options.map((item) => {
-          return <li className="h5 text-start">{item}</li>;
-        })}
-      </ul>
+      {showImage && (
+        <Container>
+          <Image src={img} width="70%"></Image>
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+      {!showImage && (
+        <ul>
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ul>
+      )}
     </Container>
   );
 };
