e5031ffb0599e7f588c3c5cf188ab16fb7f98929
Turgay Levent <turgayleventt@gmail.com>
2022-11-30 12:12:28 +0300

    language

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-  return <div>Language</div>;
+  return (
+    <Container style={{ background: "#ffdab9" }} className="p-4 rounded-4">
+      <Container>
+        <Image src={img} width="70%"></Image>
+        <h3 className="display-6">{name}</h3>
+      </Container>
+    </Container>
+  );
 };
 
 export default Language;
